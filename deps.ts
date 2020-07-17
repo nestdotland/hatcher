37156cb437ea33b06af243aa66cf03725da1eb46
oganexon <oganexon@protonmail.com>
2020-07-17 22:33:55 +0200

    chore: update cliffy to 0.11.1

--- deps.ts
+++ deps.ts
@@ -2,7 +2,7 @@ export { writeJson } from "https://deno.land/std@v0.61.0/fs/write_json.ts";
 export { readJson } from "https://deno.land/std@v0.61.0/fs/read_json.ts";
 export * as colors from "https://deno.land/std@v0.61.0/fmt/colors.ts";
 export * as semver from "https://deno.land/x/semver@v1.0.0/mod.ts";
-export { Table } from "https://deno.land/x/cliffy@v0.10.0/table.ts";
+export { Table } from "https://x.nest.land/cliffy@0.11.1/table.ts";
 export { assertEquals } from "https://deno.land/std@0.61.0/testing/asserts.ts";
 
 export * as path from "https://deno.land/std@v0.61.0/path/mod.ts";
