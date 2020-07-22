7e88136f6928933fed024fc735928c2ae9cc5b90
oganexon <oganexon@protonmail.com>
2020-07-23 00:22:49 +0200

    chore: 0.7.2

--- mod.ts
+++ mod.ts
@@ -18,7 +18,7 @@ export async function installUpdateHandler(
       "-A",
       "-n",
       module,
-      "https://x.nest.land/hatcher@0.7.1/cli.ts",
+      "https://x.nest.land/hatcher@0.7.2/cli.ts",
       executable,
       updateCheckInterval.toString(),
     ],
