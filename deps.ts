553114a57dbd6531259f8112e9b7462523345ead
oganexon <oganexon@protonmail.com>
2020-09-13 23:29:44 +0200

    fix: :ambulance: deno 1.4

--- deps.ts
+++ deps.ts
@@ -1,17 +1,16 @@
-export { writeJson } from "https://x.nest.land/std@0.65.0/fs/write_json.ts";
-
-export { readJson } from "https://x.nest.land/std@0.65.0/fs/read_json.ts";
-
-export * as colors from "https://x.nest.land/std@0.65.0/fmt/colors.ts";
-
-export * as semver from "https://deno.land/x/semver@v1.0.0/mod.ts";
-
-export { Table } from "https://x.nest.land/cliffy@0.11.1/table.ts";
+/**************** std ****************/
+export * as colors from "https://x.nest.land/std@0.69.0/fmt/colors.ts";
 
 export {
   assertEquals,
   assertMatch,
   assert,
-} from "https://x.nest.land/std@0.65.0/testing/asserts.ts";
+} from "https://x.nest.land/std@0.69.0/testing/asserts.ts";
 
-export * as path from "https://x.nest.land/std@0.65.0/path/mod.ts";
+export * as path from "https://x.nest.land/std@0.69.0/path/mod.ts";
+
+/**************** semver ****************/
+export * as semver from "https://deno.land/x/semver@v1.0.0/mod.ts";
+
+/**************** cliffy ****************/
+export { Table, Cell } from "https://x.nest.land/cliffy@0.14.1/table/mod.ts";
