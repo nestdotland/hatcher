52b19cb1d7c4b0595bed36a6053da79785a564fd
Umberto Pepato <umbopepato@users.noreply.github.com>
2021-06-09 15:26:15 +0200

    fix: semver error with Deno@1.11.0

--- deps.ts
+++ deps.ts
@@ -13,7 +13,7 @@ export { join } from "https://x.nest.land/std@0.75.0/path/mod.ts";
 export { exists } from "https://x.nest.land/std@0.75.0/fs/exists.ts";
 
 /**************** semver ****************/
-export * as semver from "https://deno.land/x/semver@v1.0.0/mod.ts";
+export * as semver from "https://deno.land/x/semver@v1.4.0/mod.ts";
 
 /**************** cliffy ****************/
 export { Table } from "https://x.nest.land/cliffy@0.14.1/table/mod.ts";
